7b6f368b5efae305c3e39086345aa3d311d852dc
buivanle <buivanle345@gmail.com>
2024-01-24 16:14:58 +0700

    Chú thích cho lần commit này

---
+++ lib/utils/Constants.dart
@@ -0,0 +1,4 @@
+class Constants {
+  static String fcmAccessToken =
+      "AAAAy7bTCW0:APA91bGOlTSgiPRiWVJCnra2dpc5zmStKueK8IWkcDU0OXnXT269v665WSglLPRktmoKLGnR1-XXU1pVp-3OX_-F5UjZXbXoJjhl5tPwg7LBNIwUtUT7Ar1wJvUVzKm-6584Td0NlJKF";
+}
